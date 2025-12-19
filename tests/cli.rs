567d4fea6f224d46f6805a0340763aaa397bb666
yilisharcs <yilisharcs@gmail.com>
2025-12-19 01:36:33 -0300

    test: add test for --global flag

--- tests/cli.rs
+++ tests/cli.rs
@@ -83,3 +83,55 @@ fn should_create_local_store_when_init_is_run() -> Result<(), Box<dyn Error>> {
 
         Ok(())
 }
+
+#[test]
+fn should_use_global_store_explicitly_when_local_exists() -> Result<(), Box<dyn Error>> {
+        let tmp = tempfile::tempdir()?;
+        let global_store = tmp.path().join("global_store");
+        fs::create_dir(&global_store)?;
+
+        let mut cmd_init = cargo_bin_cmd!("tafsk");
+        cmd_init.current_dir(tmp.path())
+                .arg("init")
+                .assert()
+                .success();
+
+        let mut cmd_add = cargo_bin_cmd!("tafsk");
+        cmd_add.current_dir(tmp.path())
+                .env("TAFSK_STORE_DIR", &global_store)
+                .arg("add")
+                .arg("--global")
+                .arg("Global Task")
+                .assert()
+                .success();
+
+        let global_entries = fs::read_dir(&global_store)?;
+        let has_global_task_created = global_entries.count() >= 2;
+        assert!(has_global_task_created);
+
+        let local_store = tmp.path().join("tasks");
+        let local_entries = fs::read_dir(&local_store)?;
+        let is_local_store_empty = local_entries.count() == 1;
+        assert!(is_local_store_empty);
+
+        let mut cmd_list_global = cargo_bin_cmd!("tafsk");
+        cmd_list_global
+                .current_dir(tmp.path())
+                .env("TAFSK_STORE_DIR", &global_store)
+                .arg("list")
+                .arg("--global")
+                .assert()
+                .success()
+                .stdout(predicate::str::contains("Global Task"));
+
+        let mut cmd_list_local = cargo_bin_cmd!("tafsk");
+        cmd_list_local
+                .current_dir(tmp.path())
+                .env("TAFSK_STORE_DIR", &global_store)
+                .arg("list")
+                .assert()
+                .success()
+                .stdout(predicate::str::contains("No tasks found"));
+
+        Ok(())
+}
